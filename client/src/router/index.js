75410020e8976d392eac9da5eb5e3d3d2341877a
ctc316 <mike.tc.chen101@gmail.com>
2019-05-02 01:31:59 -0400

    host on firebase

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -5,20 +5,16 @@ import Zone from '@/views/Zone'
 
 Vue.use(Router)
 
-const BASE_URL = process.env.NODE_ENV === 'development' ? '/' : '/zonef/'
-console.log(process.env.NODE_ENV)
-console.log(process.env.BASE_URL)
-
 export default new Router({
   mode: 'history',
   routes: [
     {
-      path: BASE_URL,
+      path: '/',
       name: 'Home',
       component: Home
     },
     {
-      path: BASE_URL + 'zone/:id',
+      path: '/zone/:id',
       name: 'Zone',
       component: Zone
     }
